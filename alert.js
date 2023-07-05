566cb3b52f74028903eed04309fc37f2a0a346fd
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-07-04 21:51:41 -0400

    w

---
+++ alert.js
@@ -0,0 +1,3 @@
+if (navigator.userAgent.indexOf('Safari', 'iPhone', 'iPod') !== -1 && navigator.userAgent.indexOf('Chrome') === -1) {
+    alert("Some features that work on some browsers do not always transfer over. There are several bugs identified and actively being fixed like the infinite load with Safari and the Peak bar not showing proper values.")
+}
